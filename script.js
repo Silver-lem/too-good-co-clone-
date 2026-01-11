6bbfe0de13206a631ded9fa523f5922d25b8824e
Geethika <april.appy24@gmail.com>
2026-01-12 00:01:16 +0530

    Locomotive Js added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function videoconAnimation(){
     var videocon = document.querySelector("#video-container")
 var playbtn = document.querySelector('#play')
@@ -47,3 +52,4 @@ function loadingAnimation(){
 })
 }
 loadingAnimation()
+
